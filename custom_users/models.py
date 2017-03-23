f4b409b74d25855a7e0c4a73c25128d5dc412d35
RobbeHeirman <robbeheirman@msn.com>
2017-03-23 14:39:21 +0100

    Initial commit

---
+++ custom_users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique = True, )
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def get_full_name(self):
+        return self.email.strip()
+
+    object = BaseUserManager
+
+
