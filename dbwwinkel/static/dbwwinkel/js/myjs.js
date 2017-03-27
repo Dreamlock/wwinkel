679ff3f94f39acc749d170b9255718e7f54214bb
RobbeHeirman <robbeheirman@msn.com>
2017-03-27 15:59:08 +0200

    bla

---
+++ dbwwinkel/static/dbwwinkel/js/myjs.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Robbe on 3/27/2017.
+ */
+
+jQuery(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.location = $(this).data("href");
+    });
+});
