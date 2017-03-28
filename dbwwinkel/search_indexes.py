7fb17c762f48508da3319df3551df62fc66b402c
Thomas Van Bogaert <t7025t@gmail.com>
2017-03-28 20:21:34 +0200

    git difficulties

---
+++ dbwwinkel/search_indexes.py
@@ -0,0 +1,16 @@
+import datetime
+from haystack import indexes
+from dbwwinkel.models import Question
+
+
+class NoteIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    # author = indexes.CharField(model_attr='user')
+    # pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return Question
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(question_text__iregex= ".")
