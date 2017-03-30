02ba1be3314231b188c94e6c88d8e4c78698cd81
Dreamlock <robbeheirman@msn.com>
2017-03-30 17:00:24 +0200

    Made some redirects

--- custom_users/views.py
+++ custom_users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
