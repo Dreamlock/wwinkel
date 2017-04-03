e248128f065bebd903aa9754984d7e6e8e2ffe3f
bdclercq <bdclercq@hotmail.com>
2017-04-03 19:29:57 +0200

    initieel script om oude database in te lezen

---
+++ script.py
@@ -0,0 +1,73 @@
+import csv, sys
+
+""" paths to csv files can be passed by commandline in following order:
+        province
+        legal entity
+        organisation
+        
+"""
+
+#path to province.csv
+with open(path) as f:
+    reader = csv.reader(f)
+    for row in reader:
+        obj, created = LegalEntity.objects.get_or_create(
+            ID=row[0],
+            name=row[1],
+            #active=row[2],
+            )
+    f.close()
+   
+#path to JuridicalEntity.csv
+with open(path) as f:
+    reader = csv.reader(f)
+    for row in reader:
+        obj, created = LegalEntity.objects.get_or_create(
+            ID=row[0],
+            entity=row[1],
+            #active=row[2],
+            )
+    f.close()
+    
+#path to organisation.csv
+with open(path) as f:
+    reader = csv.reader(f)
+    for row in reader:
+        obj, created = Address.objects.get_or_create(
+            street_name=row[],
+            street_number=row[],
+            postal_code=row[],
+            city=row[],
+            province=row[]
+            )
+        a = Address.objects.filter(street_name=row[], street_number=row[], postal_code=row[])
+        print a
+        """obj, created = Organisation.objects.get_or_create(
+            ID=row[0],
+            password=row[1],
+            organisation_name=row[2],
+            recognised_abbreviation=row[3],
+            legal_entity=row[4],
+            telephone=row[10],
+            fax=row[11],
+            website=row[12],
+            goal=row[14],
+            remarks=row[16],
+            active=row[17],
+            creationdate=row[18],
+            email=row[19],
+            
+            address=
+            )"""
+    f.close()
+        
+#path to organisation_keywords.csv
+with open(path) as f:
+    reader = csv.reader(f)
+    for row in reader:
+        obj, created = LegalEntity.objects.get_or_create(
+            ID=row[0],
+            keyword=row[1],
+            #active=row[2],
+            )
+    f.close()
