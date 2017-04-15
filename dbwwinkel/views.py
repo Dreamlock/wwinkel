5ce0e8d81a31d62c6e819d36388d380479849c3e
beau de clercq <beau de clercq>
2017-04-15 13:17:33 +0200

    html files moesten ergens anders staan, views.py moest niet worden aangepast

--- dbwwinkel/views.py
+++ dbwwinkel/views.py
@@ -78,16 +78,3 @@ def edit_question(request, question_id):
         form.save()
         return redirect(detail, question_id = '1')
     return render(request, 'dbwwinkel/vraagstelform.html',{'form': form })
-
-def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
-
-
-def handler500(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
