bc1564876c115dde4b434717a66df3ef53eab165
Thomas Van Bogaert <t7025t@gmail.com>
2017-04-20 21:04:18 +0200

    Started adding test permission backends.

--- custom_users/tests.py
+++ custom_users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from custom_users.models import *
+from custom_users.permissions_backends import *
+from dbwwinkel.models import *
 
-# Create your tests here.
+class QuestionPermissionBackendTestCase(TestCase):
+    def setUp(self):
+
+        Question.objects.create(
+            question_text='Is This Question 1?',
+            reason='Reason 1',
+            purpose='Purpose 1',
+            own_contribution='Budget contribution 1',
+            remarks='Remark 1',
+            internal_remarks='Internal remark 1',
+
+        )
