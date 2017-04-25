adc9287353e6d0ae5f6000fe01157646b581f837
Dreamlock <robbeheirman@msn.com>
2017-04-25 11:09:42 +0200

    extra models

--- dbwwinkel/models.py
+++ dbwwinkel/models.py
@@ -169,10 +169,10 @@ class Institution:
 
 class Student:
 
-    first_name = models.Charfield(max_length = 33)
+    first_name = models.CharField(max_length = 33)
     last_name = models.CharField(max_length= 45)
 
-    mobile = models.charfield(max_length = 20)
+    mobile = models.charField(max_length = 20)
     email = models.EmailField()
 
     status = models.BooleanField(default=True)
