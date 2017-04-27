c023d9a1137f7aa9aecf1275739cf0ccd3d3e0d0
bdclercq <bdclercq@hotmail.com>
2017-04-27 10:37:50 +0200

    script update

--- script.py
+++ script.py
@@ -11,7 +11,10 @@ import csv, sys
         province
         legal entity
         organisation
+        questionstatus
+        questiontypes
         question
+        questionpertype
         
 """
 
@@ -39,6 +42,66 @@ def refactorDate(cdate):
     refactored_date = "{0}-{1}-{2} {3}:{4}".format(year,month,day,hour,minute)
     return refactored_date
 
+def state_map(old_state_id):
+    idmap={
+        '1':0,
+        '2':1,
+        '3':8,
+        '4':1,
+        '5':2,
+        '6':0,
+        '7':4,
+        '8':3,
+        '9':8,
+        '10':9,
+        '11':5,
+        '12':6,
+        '13':7
+    }
+    return idmap[old_state_id]
+
+def state_id_map(state_id):
+    state_dict = {
+        1: 0,
+        2: 1,
+        3: 8,
+        4: 1,
+        5: 1,
+        6: 0,
+        7: 3,
+        8: 4,
+        9: 8,
+        10: 9,
+        11: 5,
+        12: 6,
+        13: 7
+    }
+    return state_dict[state_id]
+
+def refactor_date_time(cdate):
+    if '/' not in cdate:
+        return "0001-01-01 01:01"
+    fields = cdate.split('/')
+    day = fields[0]
+    month = fields[1]
+    year_time = fields[2].split(' ')
+    year = year_time[0]
+    hour = year_time[1].split(':')[0]
+    minute = year_time[1].split(':')[1]
+    refactored_date = "{0}-{1}-{2} {3}:{4}".format(year, month, day, hour, minute)
+    return refactored_date
+
+
+def refactor_date(cdate):
+    if '/' not in cdate:
+        return "0001-01-01"
+    fields = cdate.split('/')
+    day = fields[0]
+    month = fields[1]
+    year_time = fields[2].split(' ')
+    year = year_time[0]
+    refactored_date = "{0}-{1}-{2}".format(year, month, day)
+    return refactored_date
 
 #path to province.csv
 with open(sys.argv[1]) as f:
@@ -106,4 +169,84 @@ with open(sys.argv[3]) as f:
                 pass
     f.close()
 
+#import questionstatus
+with open(sys.argv[4]) as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if (row[0]=="idquestionregstatus"):
+            pass
+        else:
+            try:
+                print(row[0])
+                obj = dbmodels.State(id=row[0],state=state_map(row[0]))
+                print(row[1])
+                obj.save()
+            except:
+                pass
+    f.close()
+
+#import questiontypes
+with open(sys.argv[5]) as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if (row[0]=="idquestioninvestigationtype"):
+            pass
+        else:
+            try:
+                print(row[0])
+                obj = dbmodels.QuestionSubject(id=row[0],subject=row[1])
+                print(row[1])
+                obj.save()
+            except:
+                pass
+    f.close()
+
 #import question
+with open(sys.argv[6]) as f:
+    reader = csv.reader(f)
+    first_row = next(reader)
+    name_dict = dict(zip(first_row, range(len(first_row))))
+
+    def get_row(string):
+        return row[name_dict[string]]
+
+
+    for row in reader:
+        try:
+            print(get_row('datecreated'), get_row('answerwithintermtext'))
+            obj = dbmodels.Question(
+                id=get_row('idquestion'),
+                question_text=get_row('question'),
+                reason=get_row('resultuse'),
+                purpose=get_row('questionkickoff'),
+                own_contribution=get_row('costcontrib'),
+
+                remarks=get_row('remarks'),
+                internal_remarks=get_row('intakeremarks'),
+                # how_know_WW=get_row(''),
+                how_know_WW='',
+                deadline=refactor_date(get_row('answerwithintermtext')),
+
+                public=get_row('resultpublic'),
+
+                creation_date=refactor_date_time(get_row('datecreated')),
+                active=get_row('active'),
+                state=dbmodels.State.objects.get(state=state_id_map(int(get_row('reg_idquestionregstatus')))),
+
+                # region
+
+                # organisation=Organisation.objects.get(id=get_row('organizationcontact_idorganizationcontact')),
+                organisation=dbmodels.Organisation(id=0),
+
+                # keyword
+                # question_subject
+                # study_field
+            )
+            obj.save()
+        except ValueError as e:
+            if '\'NULL\'' in str(e) or '\'\'' in str(e):
+                pass
+            else:
+                pass
+                # raise
+#import questionpertype
