d52f2d7a7a1b8b09d34cc72f3ebcfbe835ae054d
Dreamlock <robbeheirman@msn.com>
2017-05-06 18:32:29 +0200

    Can change to intake now

--- dbwwinkel/views.py
+++ dbwwinkel/views.py
@@ -325,11 +325,10 @@ class StudyFieldAutocomplete(lightcomplete.Select2QuerySetView):
     def get_queryset(self):
         # Don't forget to filter out results depending on the visitor !
 
-
         qs = StudyField.objects.all()
 
         if self.q:
-            qs = qs.filter(name__istartswith=self.q)
+            qs = qs.filter(study_field__istartswith=self.q)
 
         return qs
 
