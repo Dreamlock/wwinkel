b2afef7c18521843f10e1cc1621aa21c23b10199
Thomas Van Bogaert <t7025t@gmail.com>
2017-05-20 16:36:13 +0200

    MERGE WDYFO

--- dbwwinkel/views.py
+++ dbwwinkel/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from haystack.query import SearchQuerySet
 
@@ -15,6 +15,8 @@ from custom_users.models import OrganisationUser, ManagerUser, Region
 from operator import itemgetter
 from .search import autocomplete as search, query_extra_content, query_on_states
 import os
+
+from django.views.generic import View, ListView, DetailView
 
 
 @login_required
@@ -151,13 +153,12 @@ def list_questions(request, admin_filter=None):
 
 
 def detail(request, question_id):
-
     question = Question.objects.get(id=question_id)
     organisation = question.organisation
 
     if not request.user.is_authenticated:  # Then the user is a student
-
         return student_detail(request, question, organisation)
+
     elif OrganisationUser.objects.filter(id=request.user.id).exists():
         organisation = (OrganisationUser.objects.get(id=request.user.id)).organisation
         if organisation == question.organisation:
@@ -178,9 +179,17 @@ def detail(request, question_id):
 
     return render(request, 'dbwwinkel/detail_question/detail_question_base.html', context)
 
+"""
+class QuestionDetailView(DetailView):
+    model = Question
+    template_name = 'dbwwinkel/detail_question/detail_question_base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+"""
 
 def student_detail(request, question, organisation):
-    print("hier")
     context = {'question': question,
                'organisation': organisation,
                'internal': False
