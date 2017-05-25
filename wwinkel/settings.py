c98f4047103fc1906127939e522ed150e8fa284b
Thomas Van Bogaert <t7025t@gmail.com>
2017-05-25 15:56:00 +0200

    Adjusted settings to serve static files in deployment

--- wwinkel/settings.py
+++ wwinkel/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -200,6 +199,8 @@ AUTHENTICATION_BACKENDS = (
     'custom_users.permissions_backends.QuestionPermissionsBackend',
 )
 
+LOGIN_URL = 'custom_users/login'
+
 SITE_ID = 1
 
 
@@ -224,4 +225,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_root/'
+
+STATIC_ROOT = './static_root/'
+
+STATICFILES_DIRS = ['static', 'dbwwinkel/static']
+
